f2aeb726496e5e4ef692e26af41ccdf0c53d6432
gabrielfalcaoesp <127162444+gabrielfalcaoesp@users.noreply.github.com>
2023-06-07 12:39:09 -0300

    Add files via upload

--- scapeRoom/jsScapeRoomMVC.js
+++ scapeRoom/jsScapeRoomMVC.js
@@ -12,21 +12,52 @@ var wasted = document.querySelector('.gameOver');
 var blocoQuestao = document.getElementsByClassName('blocoQuestao');
 var containerWidth = container.offsetWidth;
 var containerHeight = container.offsetHeight;
-var positionTop = parseInt(character.style.top) || character.offsetHeight + 25;
+var positionTop = parseInt(character.style.top) || character.offsetHeight ;
 var positionLeft = parseInt(character.style.left) || character.offsetWidth + 5;
-var stepSize = 30;
+var stepSize = 10;
 character.style.top = positionTop + 'px';
 character.style.left = positionLeft + 'px';
+
+// Variáveis globais
+var minutos = 20;
+var segundos = 0;
+var cronometro;
+
+// Função para atualizar o cronômetro
+function atualizarCronometro() {
+  segundos--;
+
+  if (segundos < 0) {
+    minutos--;
+    segundos = 59;
+  }
+
+  document.getElementById("minutos").textContent = ("0" + minutos).slice(-2);
+  document.getElementById("segundos").textContent = ("0" + segundos).slice(-2);
+
+  if (minutos === 0 && segundos === 0) {
+    clearInterval(cronometro);
+    wasted.style.display = 'flex'
+    setTimeout(function() {
+        location.reload();
+        }, 5000);
+  }
+}
+
+cronometro = setInterval(atualizarCronometro, 1000);
+
+
 
 // __________________________________________ Movimentação do personagem __________________________________________
 
 document.addEventListener('keydown', function (event) {
+    
     switch (event.keyCode) {
         case 37: // Left arrow key
             positionLeft = Math.max(positionLeft - stepSize, character.offsetWidth + 5);
             break;
         case 38: // Up arrow key
-            positionTop = Math.max(positionTop - stepSize, character.offsetHeight + 25);
+            positionTop = Math.max(positionTop - stepSize, character.offsetHeight );
             break;
         case 39: // Right arrow key
             positionLeft = Math.min(positionLeft + stepSize, containerWidth - character.offsetWidth);
@@ -137,7 +168,12 @@ document.addEventListener('keydown', function (event) {
         var pergunta = document.querySelector('.pergunta' + parseInt(perguntaAleatoria));
         pergunta.style.display = 'none';
 
-        if (resposta != 'C - Mundial') {
+        if (resposta != 'C - Mundial' && 
+        resposta !='A - Separar a lógica de apresentação dos dados' && 
+        resposta != 'B - Responsável pela lógica de negócio e manipulação dos dados' &&
+        resposta != 'D - Responsável pela manipulação das requisições e respostas HTTP' &&
+        resposta != 'B - Torna o código mais organizado e modular'
+        ) {
             switch (contagemVidas){
                 case 0:
                     contagemVidas ++; 
@@ -159,9 +195,10 @@ document.addEventListener('keydown', function (event) {
 
                 case 3:
                     contagemVidas ++; 
-                    console.log ("contagem de vidas = " + contagemVidas)
                     wasted.style.display = 'flex'
-                    
+                    setTimeout(function() {
+                        location.reload();
+                      }, 5000);
                     break;
             }
             
